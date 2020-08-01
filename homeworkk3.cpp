d2d9b60aaad3ee6a1f693fc896bd6e5581c77cc7
๋Janista Nonsraket <68005169+Janista09@users.noreply.github.com>
2020-08-01 14:02:54 +0700

    Add files via upload

---
+++ homeworkk3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float q1, q2, q3;
+	float mid, final, quizztotal;
+	float total, scoretotal;
+	cout << "=================QUIZZES=====================" << endl;
+
+	cout << "Enter frist quizz  (10) : ";
+	cin >> q1;
+	cout << "Enter second quizz (10) : ";
+	cin >> q2;
+	cout << "Enter third quizz  (10) : ";
+	cin >> q3;
+
+	cout << "=================MID-TERM====================" << endl;
+	cout << "Enter mid-term (40) : ";
+	cin >> mid;
+
+	cout << "=================FINAL=======================" << endl;
+	cout << "Enter final (50) : ";
+	cin >> final;
+
+	quizztotal = (q1+q2+q3) /3;
+	scoretotal = quizztotal + mid + final;
+
+	cout <<"Quizz Total : " << quizztotal << endl;
+	cout <<"Final  : " << final << endl;
+	cout <<"Total  : " << scoretotal << endl;
+	cout <<"Your score is " << (scoretotal >= 50? "PASS" : "FAL ") <<endl;
+
+	system ("pause");
+	return(0);
+}
